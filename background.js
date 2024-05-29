994cd3081521ddfc59fc2abca2f4cb34c1cfbd39
lizhaowen <690591397@qq.com>
2024-05-29 17:17:37 +0800

    拦截header

--- background.js
+++ background.js
@@ -4,8 +4,73 @@ chrome.runtime.onMessage.addListener(
       console.info('content.js normal operation');
     }
     if (request.greeting === "hello") {
-      sendResponse({farewell: "hi"});
+      sendResponse({ farewell: "hi" });
     }
     return true;
   }
 );
+
+const allResourceTypes = 
+    Object.values(chrome.declarativeNetRequest.ResourceType);
+const MY_CUSTOM_RULE_ID = 1
+
+chrome.declarativeNetRequest.updateDynamicRules({
+  removeRuleIds: [MY_CUSTOM_RULE_ID],
+  addRules: [
+    {
+      id: MY_CUSTOM_RULE_ID,
+      priority: 1,
+      action: {
+        type: "modifyHeaders",
+        requestHeaders: [
+          // {
+          //   operation: "set",
+          //   header: "x-respond-with",
+          //   value: "html"
+          // },
+          // {
+          //   operation: "set",
+          //   header: "X-With-Generated-Alt",
+          //   value: "true"
+          // }
+          {
+            operation: "set",
+            header: "X-Return-Format",
+            value: "screenshot"
+          },
+        ]
+      },
+      condition: {
+        urlFilter: "r.jina.ai",
+        resourceTypes: allResourceTypes
+      },
+    }
+  ],
+});
+
+// chrome.webRequest.onBeforeSendHeaders.addListener(
+//   function(details) {
+//     var headers = details.requestHeaders;
+//     var headerExists = false;
+
+//     console.info('检查header信息...');
+//     // 检查是否已存在
+//     for (var i = 0; i < headers.length; ++i) {
+//       if (headers[i].name.toLowerCase() === 'x-respond-with') {
+//         headerExists = true;
+//         headers[i].value = 'html';  // 如果存在，更新它
+//         break;
+//       }
+//     }
+
+//     // 如果header不存在，添加它
+//     if (!headerExists) {
+//       headers.push({ name: 'x-respond-with', value: 'html' });
+//     }
+
+//     return { requestHeaders: headers };
+//   },
+//   { urls: ["*://r.jina.ai/*"] },  // 仅对https://r.jina.ai的URL有效
+//   ["blocking", "requestHeaders"]
+// );
+
--- index.js
+++ index.js
@@ -1,71 +1,68 @@
-document.querySelector('#resHTML').addEventListener('click', () => {
-	try {
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			console.info('current tab url is:', tabs[0].url);
-			chrome.tabs.update(
-				tabs[0].id,
-				{
-					url: `https://hawkeye-jina-redirect-file.deno.dev/?targetURL=${encodeURIComponent(tabs[0].url)}`
-				}
-			);
-		});
-
-		// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		// 	console.info(tabs[0].url);
-	
-		// 	chrome.scripting.executeScript({
-		// 		target: {tabId: tabs[0].id},
-		// 		function: () => {
-		// 			generateIframe(tabs[0].url);
-		// 		}
-		// 	}, function(results) {
-		// 		// 这里的results是一个数组，包含了每个执行环境的执行结果
-		// 		console.log('code res:', results);
-		// 	});
-		// });
-	} catch (error) {
-		console.warn('scripting.executeScript 操作error: ', error);
-	}
+document.querySelector('#usePrefix').addEventListener('click', () => {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			var currentTab = tabs[0]; // 在当前窗口中获取活动标签
+			console.info(currentTab.url); // 输出当前标签的URL
+			var newUrl = `https://r.jina.ai/${currentTab.url}`; // 您想要导航到的新URL
+			console.info(`newUrl: ${newUrl}`);
+			chrome.tabs.update(currentTab.id, {url: newUrl});
+	});
 });
 
-function generateIframe(url) {
-	const body = document.querySelector('body');
-	body.style.position = 'relative';
+// document.querySelector('#resHTML').addEventListener('click', () => {
+// 	try {
+// 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// 			console.info('current tab url is:', tabs[0].url);
+// 			chrome.tabs.update(
+// 				tabs[0].id,
+// 				{
+// 					url: `https://hawkeye-jina-redirect-file.deno.dev/?targetURL=${encodeURIComponent(tabs[0].url)}`
+// 				}
+// 			);
+// 		});
 
-	const iframe = document.createElement('iframe');
-	function setIframeStyles() {
-		iframe.style.width = '100%';
-		iframe.style.height = '100%';
-		iframe.style.position = 'absolute';
-		iframe.style.top = 0;
-		iframe.style.left = 0;
-		iframe.style.zIndex = 100;
-		iframe.style.backgroundColor = '#FFFFFF';
-	}
+// 	} catch (error) {
+// 		console.warn('scripting.executeScript 操作error: ', error);
+// 	}
+// });
 
-	const newUrl = `https://r.jina.ai/${url}`;
-	// Unchecked runtime.lastError: Frame with ID 0 is showing error page 不能修改chrome:// 协议的页面
+// function generateIframe(url) {
+// 	const body = document.querySelector('body');
+// 	body.style.position = 'relative';
 
-	fetch(newUrl, {
-		method: 'GET',
-		headers: {
-			'x-respond-with': 'html',
-		},
-	}).then(response => {
-		if (!response.ok) {
-			throw new Error('网络请求错误，状态码：' + response.status);
-		}
-		return response.text();
-	}).then(data => {
-		const blob = new Blob([data], {type: 'text/html'});
-		const blobUrl = URL.createObjectURL(blob);
-		console.info('新地址:', blobUrl);
+// 	const iframe = document.createElement('iframe');
+// 	function setIframeStyles() {
+// 		iframe.style.width = '100%';
+// 		iframe.style.height = '100%';
+// 		iframe.style.position = 'absolute';
+// 		iframe.style.top = 0;
+// 		iframe.style.left = 0;
+// 		iframe.style.zIndex = 100;
+// 		iframe.style.backgroundColor = '#FFFFFF';
+// 	}
+
+// 	const newUrl = `https://r.jina.ai/${url}`;
+// 	// Unchecked runtime.lastError: Frame with ID 0 is showing error page 不能修改chrome:// 协议的页面
+
+// 	fetch(newUrl, {
+// 		method: 'GET',
+// 		headers: {
+// 			'x-respond-with': 'html',
+// 		},
+// 	}).then(response => {
+// 		if (!response.ok) {
+// 			throw new Error('网络请求错误，状态码：' + response.status);
+// 		}
+// 		return response.text();
+// 	}).then(data => {
+// 		const blob = new Blob([data], {type: 'text/html'});
+// 		const blobUrl = URL.createObjectURL(blob);
+// 		console.info('新地址:', blobUrl);
 		
-		iframe.src = blobUrl;
-		body.appendChild(iframe);
-		setIframeStyles();
-	}).catch(error => {
-		console.error('请求失败:', error);
-	});
-}
+// 		iframe.src = blobUrl;
+// 		body.appendChild(iframe);
+// 		setIframeStyles();
+// 	}).catch(error => {
+// 		console.error('请求失败:', error);
+// 	});
+// }
 
